0541af5a8815b1bb7ba02b32c63f89914812026a
Bruno Zabala <brunozabalap@gmail.com>
2024-09-20 14:57:28 -0600

    Main code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <fstream> 
 #include <iostream>
+#include <Windows.h>
 
-#include "modules/sort.h"
+#include "modules/sort_search.h"
 
 int main(int argc, char* argv[]){
     // Lee la ruta del archivo desde la terminal
     ifstream file(argv[1]);
 
     // Crea (o escribe sobre) los archivos de salida
-    ofstream search_results("outputs/search_results.txt");
     ofstream sorted_orders("outputs/sorted_orders.txt");
 
+    int option;
     int i = 0;
+
+    string start_date;
+    string end_date;
+    int start_date_value;
+    int end_date_value;
 
     // Variable auxiliar para leer cada linea del archivo
     string line;
@@ -41,15 +47,54 @@ int main(int argc, char* argv[]){
 
     cout << endl << endl;
 
-    cout << "-----------------------------" << endl;
-    cout << "Busqueda de ordenes por fecha" << endl;
-    cout << "-----------------------------" << endl;
-    cout << "Ingrese el limite menor: " << endl;
-    cout << ">";
+    // Busqueda de ordenes por fecha
+    cout << "------------------------------------" << endl;
+    cout << "Busqueda de ordenes por fecha y hora" << endl;
+    cout << "------------------------------------" << endl;
+    cout << "Seleccione una opcion: " << endl;
+    cout << "1- Busqueda individual" << endl << "2- Busqueda por rango" << endl;
+    cout << "> ";
+
+    cin >> option;
+
+    Sleep(1000);
+
+    switch(option){
+    case 1:
+        cout << endl << "Ingrese la fecha y hora a buscar (ej. \"Jun 12 06:51:16\"):" << endl;
+        
+        cout << "> ";
+        getline(cin >> ws, start_date);
+        start_date_value = get_value(start_date);
+
+        cout << endl;
+
+        single_search(start_date, start_date_value, orders, i);
+
+        break;
+    
+    case 2:
+        cout << endl << "Ingrese las fechas y horas (ej. \"Jun 12 06:51:16\"):" << endl;
+        
+        cout << "Limite inferior: ";
+        getline(cin >> ws, start_date);
+        start_date_value = get_value(start_date);
+
+        cout << "Limite inferior: ";
+        getline(cin >> ws, end_date);
+        end_date_value = get_value(end_date);
+
+        range_search(start_date, end_date, start_date_value, end_date_value, orders, i);
+
+        break;
+
+    default:
+        cout << "Opcion invalida" << endl;
+        break;
+    }
 
     // Cierra los archivos utilizados en el programa
     file.close();
-    search_results.close();
     sorted_orders.close();
 
     return 0;
